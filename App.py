3b56551cdd8561f76330c1284342daeed18c7186
kelvindev2017 <36997730+kelvindev2017@users.noreply.github.com>
2026-09-13 16:36:02 +0200

    bug fixed

--- App.py
+++ App.py
@@ -233,10 +233,12 @@ if check_password():
                 buy_signal = False
                 sell_signal = False
             
-                if (not in_trade) and buy_condition.ilocin_trade == True:
+                # Enter trade
+                if (not in_trade) and buy_condition.ilocin_trade = True
                     buy_signal = True
             
-                elif in_trade and sell_condition.ilocin_trade == False:
+                # Exit trade
+                elif in_trade and sell_condition.ilocin_trade = False
                     sell_signal = True
             
                 position.append(
